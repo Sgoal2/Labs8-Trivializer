72315483061b3788cef529eb6a8bc2aa148210b1
cmok4290 <cmok4290@gmail.com>
2018-11-06 12:31:10 -0500

    setup initial GamesList template

---
+++ Frontend/trivializer/src/components/GamesList.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Games from "./Games";
+
+class GamesList extends Component {
+    render() {
+        return (
+            <div>
+                GamesList
+                <Games />
+            </div>
+        );
+    }
+}
+
+export default GamesList;
