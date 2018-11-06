9f7f0cbbd4dc6634c678eeae715de6578dac4b73
cmok4290 <cmok4290@gmail.com>
2018-11-06 12:50:51 -0500

    setup initial Games template

---
+++ Frontend/trivializer/src/components/Games.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Rounds from "./Rounds";
+
+class Games extends Component {
+    render() {
+        return (
+            <div>
+                Games
+                <Rounds />
+            </div>
+        );
+    }
+}
+
+export default Games;
