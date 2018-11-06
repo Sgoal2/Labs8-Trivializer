eca9a608ef455f574216c0305110a530276f6539
Nicky Chen <nicky.chen0@gmail.com>
2018-11-06 09:19:51 -0800

    Set up components templates, add bootstrap, redux templates

---
+++ Frontend/trivializer/src/components/LandingPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class LandingPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return <React.Fragment>Empty Landing Page</React.Fragment>;
+  }
+}
+
+export default LandingPage;
