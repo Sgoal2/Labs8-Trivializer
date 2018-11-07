4888c526eca4f2ad860ec35f79a1dcbf0c8b1d76
Nicky Chen <nicky.chen0@gmail.com>
2018-11-07 15:25:50 -0800

    Add breadcrumbs for each page

--- Frontend/trivializer/src/components/Billing.js
+++ Frontend/trivializer/src/components/Billing.js
@@ -1,12 +1,30 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { Link } from "react-router-dom";
 
 const Billing = () => {
   return (
     <div className="billing-page">
-      <Navbar />
+      <div className="top-content">
+        <div className="top-leftside">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item">
+                <Link to="/">Home</Link>
+              </li>
+              <li class="breadcrumb-item active" aria-current="page">
+                Billing
+              </li>
+            </ol>
+          </nav>
+        </div>
+        <Link className="top-rightside" to="/">
+          Sign Out
+        </Link>
+      </div>
       <div className="main-content">
-        <h1>Billing Page</h1>
+        <Navbar />
+        <h1 className="main-middle">Billing Page</h1>
       </div>
     </div>
   );
--- Frontend/trivializer/src/components/Invoices.js
+++ Frontend/trivializer/src/components/Invoices.js
@@ -1,12 +1,30 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { Link } from "react-router-dom";
 
 const Invoices = () => {
   return (
     <div className="invoices-page">
-      <Navbar />
+      <div className="top-content">
+        <div className="top-leftside">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item">
+                <Link to="/">Home</Link>
+              </li>
+              <li class="breadcrumb-item active" aria-current="page">
+                Invoices
+              </li>
+            </ol>
+          </nav>
+        </div>
+        <Link className="top-rightside" to="/">
+          Sign Out
+        </Link>
+      </div>
       <div className="main-content">
-        <h1>Invoices Page</h1>
+        <Navbar />
+        <h1 className="main-middle">Invoices Page</h1>
       </div>
     </div>
   );
