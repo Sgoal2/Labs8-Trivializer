c1fb1cc53b77d04009f75e7592cd8a79ccd7b039
cmok4290 <cmok4290@gmail.com>
2018-11-08 15:09:05 -0500

    add initial layout and nav

--- Frontend/trivializer/src/components/Game.js
+++ Frontend/trivializer/src/components/Game.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Navbar from "./Navbar";
+import { Link } from "react-router-dom";
 
 class Game extends Component {
     constructor(props) {
@@ -7,7 +9,34 @@ class Game extends Component {
     }
 
     render() {
-        return null;
+        return (
+            <div className="game-page">
+                <div className="top-content">
+                    <div className="top-leftside">
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item">
+                                    <Link to="/">Home</Link>
+                                </li>
+                                <li
+                                    class="breadcrumb-item active"
+                                    aria-current="page"
+                                >
+                                    Games
+                                </li>
+                            </ol>
+                        </nav>
+                    </div>
+                    <Link className="top-rightside" to="/">
+                        Sign Out
+                    </Link>
+                </div>
+
+                <div className="main-content">
+                    <Navbar />
+                </div>
+            </div>
+        );
     }
 }
 
