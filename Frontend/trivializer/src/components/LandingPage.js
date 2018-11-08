166dc46dcb87e4044ee118792c652e49237dcf2f
Nicky Chen <nicky.chen0@gmail.com>
2018-11-08 12:34:15 -0800

    testing continuous deployment

--- Frontend/trivializer/src/components/LandingPage.js
+++ Frontend/trivializer/src/components/LandingPage.js
@@ -252,7 +252,7 @@ class LandingPage extends React.Component {
                 Users who register get a welcome email and can reset their password via email as
                 well.
               </p>
-              <Link to="/billing" className="btn btn-primary">
+              <Link to="/billing" className="btn btn-secondary">
                 Buy Now
               </Link>
             </div>
