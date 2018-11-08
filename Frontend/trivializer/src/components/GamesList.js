bb73c61a5e970c6b5695dc7853b79e6c5e7e396a
cmok4290 <cmok4290@gmail.com>
2018-11-08 12:45:08 -0500

    pass gamesList state to games

--- Frontend/trivializer/src/components/GamesList.js
+++ Frontend/trivializer/src/components/GamesList.js
@@ -4,35 +4,55 @@ import Navbar from "./Navbar";
 import { Link } from "react-router-dom";
 
 class GamesList extends Component {
-  render() {
-    return (
-      <div className="gameslist-page">
-        <div className="top-content">
-          <div className="top-leftside">
-            <nav aria-label="breadcrumb">
-              <ol class="breadcrumb">
-                <li class="breadcrumb-item">
-                  <Link to="/">Home</Link>
-                </li>
-                <li class="breadcrumb-item active" aria-current="page">
-                  Games
-                </li>
-              </ol>
-            </nav>
-          </div>
-          <Link className="top-rightside" to="/">
-            Sign Out
-          </Link>
-        </div>
+    // temp state, move to redux later
+    constructor() {
+        super();
+        this.state = {
+            gamesList: [
+                { name: "Game 1", description: "Game 1" }
+                // { name: "Game 2", description: "Game 2" }
+            ]
+        };
+    }
 
-        <div className="main-content">
-          <Navbar />
-          <h3 className="main-middle">Add New Game</h3>
-          {/* Ternary here should go: if [games] display <Games /> component, if NOT, display the add new game sign*/}
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="gameslist-page">
+                <div className="top-content">
+                    <div className="top-leftside">
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item">
+                                    <Link to="/">Home</Link>
+                                </li>
+                                <li
+                                    class="breadcrumb-item active"
+                                    aria-current="page"
+                                >
+                                    Games
+                                </li>
+                            </ol>
+                        </nav>
+                    </div>
+                    <Link className="top-rightside" to="/">
+                        Sign Out
+                    </Link>
+                </div>
+
+                <div className="main-content">
+                    <Navbar />
+                    {/* Ternary here should go: if [games] display <Games /> component, if NOT, display the add new game sign*/}
+                    {this.state.gamesList.length < 1 ? (
+                        <h3 className="main-middle">Add New Game</h3>
+                    ) : (
+                        this.state.gamesList.map((item, i) => (
+                            <Games game={item} />
+                        ))
+                    )}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default GamesList;
